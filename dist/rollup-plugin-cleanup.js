687061fa702c8a1f6078abb25924c906785f0858
Martin Kühl <martin.kuehl@gmail.com>
2018-04-10 12:18:08 +0200

    Update dist/

--- dist/rollup-plugin-cleanup.js
+++ dist/rollup-plugin-cleanup.js
@@ -176,7 +176,7 @@ function blankComments(code, file, options) {
 }
 
 const EOL_TYPES   = { unix: '\n', mac: '\r', win: '\r\n' };
-const FIRST_LINES = /^(\s*[\r\n])\s*\S/;
+const FIRST_LINES = /^\s*[\r\n]/;
 const EACH_LINE   = /.*(?:\r\n?|\n)/g;
 const TRIM_SPACES = /[^\S\r\n]+$/;
 
@@ -187,10 +187,13 @@ function removeLines(magicStr, code, file, options) {
 
   const eolTo   = EOL_TYPES[options.normalizeEols];
   const empties = options.maxEmptyLines;
+  const maxEolCharsAtStart = empties < 0 ? Infinity : empties ? empties * eolTo.length : 0;
+  // middle lines count one more
+  const maxEolChars = maxEolCharsAtStart + eolTo.length;
 
-  let maxEolChars = empties < 0 ? Infinity : empties ? empties * eolTo.length : 0;
-  let match, block;
+  let match, block, region;
   let changes = false;
+  let pos = 0;
 
   // Helpers
   // -------
@@ -202,46 +205,62 @@ function removeLines(magicStr, code, file, options) {
     }
   };
 
-  const limitLines = (str) => {
+  const limitLines = (str, max) => {
     let ss = str.replace(EACH_LINE, eolTo);
-    if (ss.length > maxEolChars) {
-      ss = ss.slice(0, maxEolChars);
+    if (ss.length > max) {
+      ss = ss.slice(0, max);
     }
     return ss
+  };
+
+  const squashRegion = (start, end, atStart, atEnd) => {
+    NEXT_LINES.lastIndex = 0;
+    region = magicStr.slice(start, end);
+
+    // first empty lines
+    if (atStart && (match = region.match(FIRST_LINES))) {
+      block = match[0];
+      replaceBlock(block, start, limitLines(block, maxEolCharsAtStart));
+      NEXT_LINES.lastIndex = block.length;
+    }
+
+    if (empties) {
+      // maxEmptyLines -1 or > 0
+      while (match = NEXT_LINES.exec(region)) {
+        block = match[0];
+        replaceBlock(block, start + match.index, limitLines(block, maxEolChars));
+      }
+    } else {
+      // removes all the empty lines
+      while (match = NEXT_LINES.exec(region)) {
+        replaceBlock(match[0], start + match.index, eolTo);
+      }
+    }
+
+    if (atEnd && (match = TRIM_SPACES.exec(region))) {
+      replaceBlock(match[0], start + match.index, '');
+    }
+  };
+
+  const onToken = (ref) => {
+    var start = ref.start;
+    var end = ref.end;
+    var type = ref.type;
+
+    if (pos !== start) {
+      squashRegion(pos, start, pos === 0, type === acorn.tokTypes.eof);
+    }
+    pos = end;
   };
 
   // Lines remotion
   // --------------
 
-  // first empty lines
-  match = code.match(FIRST_LINES);
-  if (match) {
-    block = match[1];
-    replaceBlock(block, 0, limitLines(block));
-    NEXT_LINES.lastIndex = match[0].length;
-  }
-
-  // middle lines count one more
-  maxEolChars += eolTo.length;
-
-  if (empties) {
-    // maxEmptyLines -1 or > 0
-    while ((match = NEXT_LINES.exec(code))) {
-      block = match[0];
-      replaceBlock(block, match.index, limitLines(block));
-    }
-  } else {
-    // removes all the empty lines
-    while ((match = NEXT_LINES.exec(code))) {
-      replaceBlock(match[0], match.index, eolTo);
-    }
-  }
-
-  // now, trim the last spaces not handled by previous regex
-  match = code.match(TRIM_SPACES);
-  if (match) {
-    replaceBlock(match[0], match.index, '');
-  }
+  acorn.parse(code, {
+    ecmaVersion: options.ecmaVersion,
+    sourceType: options.sourceType,
+    onToken,
+  });
 
   return changes
 }
